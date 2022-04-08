aa7985c34e3572f0e02514dd54df1403aab07e23
Roman Putintsev <putincevra@sushivesla.team>
2022-04-08 19:02:12 +0300

    1

---
+++ MyPod/Greater.swift
@@ -0,0 +1,12 @@
+//
+//  Greater.swift
+//  MyPod
+//
+//  Created by Роман Путинцев on 08.04.2022.
+//
+
+import Foundation
+
+public final class Greater {
+    func greating() -> String { return "Hello" }
+}
---
+++ MyPod/MyPod.h
@@ -0,0 +1,18 @@
+//
+//  MyPod.h
+//  MyPod
+//
+//  Created by Роман Путинцев on 08.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyPod.
+FOUNDATION_EXPORT double MyPodVersionNumber;
+
+//! Project version string for MyPod.
+FOUNDATION_EXPORT const unsigned char MyPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPod/PublicHeader.h>
+
+
